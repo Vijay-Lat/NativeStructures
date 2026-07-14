3955ee25e35159367b948ff931d1f71ecff66543
Vijay-Lat <vijayserpentine@gmail.com>
2026-07-15 00:33:04 +0530

    Add Cookie

---
+++ Cookie.java
@@ -0,0 +1,11 @@
+public class Cookie {
+    private String color;
+
+    public Cookie (String color){
+        this.color = color ;
+    }
+
+    public String getColor (){
+        return this.color;
+    }
+}
