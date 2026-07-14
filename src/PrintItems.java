9fbe9ca94aeb86a922a13710bc779e79fc133277
Vijay-Lat <vijayserpentine@gmail.com>
2026-07-14 23:36:53 +0530

    Add java DSA files

---
+++ src/PrintItems.java
@@ -0,0 +1,13 @@
+public class PrintItems {
+    PrintItems(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
+
+        for (int j = 0; j < n; j++) {
+            System.out.println(j);
+        }
+    }
+
+}
+
