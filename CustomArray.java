a19775771c46fe2b5b461ef0e2a04be6c9543481
Vijay-Lat <vijayserpentine@gmail.com>
2026-07-19 23:56:01 +0530

    First Code

---
+++ CustomArray.java
@@ -0,0 +1,10 @@
+public class CustomArray {
+
+    public static void main(String[] args) {
+        int[] myNumbers = new int[5];
+        myNumbers[4] = 23;
+        myNumbers[1] = 84632;
+        myNumbers[2] = 87786;
+        System.out.println("This is the value"+" "+myNumbers[0]);
+    }
+}
