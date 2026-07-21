80cfea008f0b6717bcbc84b56e9037ad65517f8a
Vijay-Lat <vijayserpentine@gmail.com>
2026-07-21 23:15:12 +0530

    Static Methods

--- JAVA_DUKE/HelloWorld.java
+++ JAVA_DUKE/HelloWorld.java
@@ -8,19 +8,38 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class HelloWorld {
-   
-    public static void main(String[] args) {
-        try {
-            List <String> allLines = Files.readAllLines(Paths.get("JAVA_DUKE/helloWorld.txt"));
 
-           
+    public static void main(String[] args) {
+        System.out.println(g());
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get("JAVA_DUKE/helloWorld.txt"));
+
             for (int i = 0; i < allLines.size(); i++) {
-    // Crucial part: You must use .get(i) for a List!
-    String line = allLines.get(i); 
-    System.out.println(line);
-}
+                // Crucial part: You must use .get(i) for a List!
+                String line = allLines.get(i);
+                System.out.println(line);
+            }
 
         } catch (IOException e) {
         }
+    }
+
+    static int myFunction(int x, int y) {
+        int z = 2 * x - y;
+        return z * x;
+    }
+
+    static int f(int n) {
+        return 3 + myFunction(n, n + 1);
+    }
+
+    static int g() {
+        int a;
+        a = myFunction(3, 7);
+        int b;
+        b = f(a * a);
+
+        return b;
+
     }
 }
